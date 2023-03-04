ff3f21dfc7002653a8914514e0d808b503e616bc
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-03-04 10:41:54 +0530

    Add files via upload

---
+++ Data types.js
@@ -0,0 +1,12 @@
+var myname="Sparsh"
+console.log(myname)
+var no=44
+console.log(no)
+//type of operator
+console.log(typeof(myname))
+console.log(typeof(no))
+
+var iamsparsh= true
+console.log(iamsparsh)
+console.log(typeof(iamsparsh))
+console.log(typeof(sparsh))  // undefined
---
+++ value and variables.js
@@ -0,0 +1,7 @@
+var myname = 'sparsh';
+//var is variable(key) myname is variable name and value is written in '' or ""
+console.log(myname)
+var myage=22
+console.log(myage)
+var $s=123
+console.log($s)
