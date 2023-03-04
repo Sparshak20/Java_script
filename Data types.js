aaeda945c37c0a8ed15806fc0778dfbecb4e5067
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-03-04 11:03:55 +0530

    Updated

--- Data types.js
+++ Data types.js
@@ -10,3 +10,17 @@ var iamsparsh= true
 console.log(iamsparsh)
 console.log(typeof(iamsparsh))
 console.log(typeof(sparsh))  // undefined
+
+
+console.log(10+"20")  //1020 concatenate
+console.log(9-"5")  // 4 bug
+console.log("Java " + "Script"); //Java Script
+console.log(""+"") //blank
+console.log("Spa"-"See")  //NaN not a number
+
+// In javascript 1 represents true
+// and 0 represents false
+console.log(true +true) //2 (1+1)
+console.log(true+false) //1
+console.log(false+true) //1
+console.log(false+false) //0
