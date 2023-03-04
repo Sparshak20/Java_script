6a33f4f8a66e0dc2cd09d8bdb283bca99eaf8639
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-03-04 11:04:46 +0530

    Add files via upload

---
+++ diff between null and undefined.js
@@ -0,0 +1,19 @@
+// NULL and Undefined
+
+var iamuseless=null
+console.log(iamuseless)
+console.log(typeof(iamuseless)) //object  bug
+
+var iamstandby// can be used as replacement(role add ni kia )
+console.log(iamstandby)
+
+//What is NaN
+// NaN is a property of global object
+// In other words it is a variable in global scope
+// The initial value of NaN is Not - A - Number
+
+var myno=9138
+var myname="abd"
+
+console.log(isNaN(myname)); //true
+console.log(isNaN(myno));  //false
