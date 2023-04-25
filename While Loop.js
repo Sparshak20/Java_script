ff6f04e449f5cbf0d1054fe9e71e40348db82c03
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-04-25 23:46:26 +0530

    Add files via upload

---
+++ While Loop.js
@@ -0,0 +1,20 @@
+//While loop
+//The while statement creates a loop that executes a specified statement
+//as long as the test condition evaluates to true
+
+var n=1;
+while(n<11)
+{
+    console.log(+n)
+    n++;
+}
+
+
+// Do while loop
+
+var num=0
+do{
+    console.log(num)
+    num++;
+
+}while(num<=10)
