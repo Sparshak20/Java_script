29da825a299603a18bd0124dd19c52b7dc72dc81
Sparshak20 <76407421+Sparshak20@users.noreply.github.com>
2023-04-25 23:45:35 +0530

    Add files via upload

---
+++ For loop.js
@@ -0,0 +1,15 @@
+// //for(initializer;condition;iteration)
+// // {
+// //     code to be executed;
+// // }
+// for(var num=10;num<=20;num++)
+// {
+// console.log(num);
+// }
+
+//WAP to print table for a given number
+var num=10;
+for(var nums=1;nums<=10;nums++)
+{
+    console.log(nums*num);
+}
